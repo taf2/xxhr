bf4757e99c64bcc2eb678f78d4600447621e151d
Todd Fisher <taf2@glazgo.(none)>
2009-03-27 15:29:09 -0400

    cleanup

--- xajax-worker.js
+++ xajax-worker.js
@@ -66,23 +66,6 @@ Request.pool.onmessage = function(a, b, message ) {
   //Request.pool.sendMessage({res:"Invalid client origin:" + message.origin}, message.sender);
   Request.onmessage(message);
 }
-/*
-wp.onmessage = function(a, b, message) {
-  var origin = message.origin;
-  if( origin != clientDomain ) {
-    wp.sendMessage({res:"Invalid client origin:" + origin}, message.sender);
-    return;
-  }
-  wp.sendMessage({res:"starting request"}, message.sender);
-  var request = google.gears.factory.create('beta.httprequest');
-  request.open("GET", "http://127.0.0.1:2001/test");
-  request.onreadystatechange = function() {
-    if( request.readyState == 4 ) {
-      wp.sendMessage({res:request.responseText}, message.sender);
-    }
-  }
-  request.send();
-};*/
 
 Function.prototype.bind = function() {
   // copy arguments
